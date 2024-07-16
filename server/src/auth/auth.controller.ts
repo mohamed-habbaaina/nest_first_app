3bc04af93a29c9335c513a7de6b5142176734ac3
@mohamed.habaina <mohamed.habaina@gmail.com>
2024-07-16 10:44:32 +0200

    JWT update controller

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,13 +1,29 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
-import { AuthService } from './auth.service';
-
-@Controller('auth')
-export class AuthController {
+import {
+    Body,
+    Controller,
+    Get,
+    HttpCode,
+    HttpStatus,
+    Post,
+    Request,
+    UseGuards
+  } from '@nestjs/common';
+  import { AuthGuard } from './auth.guard';
+  import { AuthService } from './auth.service';
+  
+  @Controller('auth')
+  export class AuthController {
     constructor(private authService: AuthService) {}
-
+  
     @HttpCode(HttpStatus.OK)
     @Post('login')
     signIn(@Body() signInDto: Record<string, any>) {
       return this.authService.signIn(signInDto.username, signInDto.password);
     }
-}
+  
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+      return req.user;
+    }
+  }  
